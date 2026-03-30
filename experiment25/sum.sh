9001878ed16b0ec50372988acef68cea032ac513
Krishna-design396 <sini24362@gmail.com>
2026-03-30 16:02:09 +0530

    Delete experiment25 directory

--- experiment25/sum.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-add()  #function definition
-{
-    sum=$(( $1 + $2 ))   #$1 – 1st argument & $1 – 2nd argument
-    echo "Sum = $sum"
-}
-#main 
-read -p " Enter two numbers: " a b
-add $a $b  #function call
